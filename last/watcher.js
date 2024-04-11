cb36441288dd22fe618618b194d1c906e82d6543
phukon <hello@rikiphukon.com>
2024-04-12 04:40:36 +0530

    check

--- last/watcher.js
+++ last/watcher.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { fork } from 'child_process';
+import { fork, spawn } from 'child_process';
 import { fileURLToPath } from 'url';
 import chokidar from 'chokidar';
 import { waitForDirectoryCreation } from './wait-creation.js';
@@ -12,6 +12,217 @@ const { tmpdir: tempDir } = os;
 let CHILD_READY_FLAG = false;
 let timeout;
 console.log(timeout);
+
+
+const childCode = `
+import blessed from \'blessed\';
+import { fileURLToPath } from \'url\';
+import path from \'path\';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/**
+ * Global store to hold counts for each directory.
+ */
+const directoryCounts = {};
+
+/**
+ * Resets the timeout to 20 seconds.
+ * This is added as a redundancy in case the child process runs in a detached mode
+ * and doesn\'t die on the parent being killed.
+ */
+let timeout;
+function resetTimeout() {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    logMessage(\'No activity detected for over 10 seconds. Killing process...\');
+    process.exit(1);
+  }, 20_000);
+}
+
+/**
+ * Creates a blessed screen with a menu box and a log box.
+ */
+const screen = blessed.screen({
+  smartCSR: true,
+  title: \'Terminal Interface\',
+});
+
+const menuBox = blessed.box({
+  parent: screen,
+  width: \'80%\',
+  height: \'100%\',
+  border: {
+    type: \'line\',
+  },
+  style: {
+    fg: \'white\',
+    border: {
+      fg: \'white\',
+    },
+  },
+});
+
+const menu = blessed.list({
+  parent: menuBox,
+  top: \'center\',
+  left: \'center\',
+  width: \'90%\',
+  height: \'90%\',
+  align: \'center\',
+  tags: true,
+  keys: true,
+  border: {
+    type: \'line\',
+  },
+  style: {
+    selected: {
+      bg: \'yellow\',
+      bold: true,
+    },
+  },
+  items: [\'Use the\', \'arrow keys\', \'to move\'],
+});
+
+menuBox.pushLine(\'\\t\\tTemplate pages\');
+
+const logBox = blessed.box({
+  parent: screen,
+  left: \'80%\',
+  width: \'20%\',
+  height: \'80%\',
+  border: {
+    type: \'line\',
+  },
+  scrollbar: {
+    ch: \' \'
+  },
+  style: {
+    fg: \'white\',
+    bg: \'black\',
+    border: {
+      fg: \'white\',
+    },
+  },
+});
+
+const totalBox = blessed.box({
+  parent: screen,
+  top: \'80%\',
+  left: \'80%\',
+  width: \'20%\',
+  height: \'20%\',
+  border: {
+    type: \'line\',
+  },
+  scrollbar: {
+    ch: \' \'
+  },
+  style: {
+    fg: \'white\',
+    bg: \'black\',
+    border: {
+      fg: \'white\',
+    },
+  },
+});
+
+/**
+ * Logs a message to the log box and renders the screen.
+ * @param {string} message - The message to log.
+ */
+function logMessage(message) {
+  logBox.setContent(\'\');
+  logBox.pushLine(message);
+  screen.render();
+}
+
+/**
+ * Event handler for menu selection.
+ * If the selected item is \'Quit\', the process exits.
+ * Otherwise, it logs the selected route and the number of pages built for that route.
+ * @param {object} item - The selected menu item.
+ */
+menu.on(\'select\', (item) => {
+  if (item.content === \'Quit\') {
+    return process.exit(0);
+  }
+  logMessage(\`\\nSelected route:\\n \${item.content}\`);
+  const count = directoryCounts[item.content];
+  if (count !== undefined) {
+    logMessage(\`\\nPages built for \\n \${item.content}\\n\\n > \${count}\`);
+  } else {
+    logMessage(\`\\nPages built for \\n \${item.content}\\n\\n > 0\`);
+  }
+});
+
+// Handle Escape or Q to quit
+screen.key([\'escape\', \'q\', \'C-c\'], () => {
+  return process.exit(0);
+});
+
+// Initial log message
+logBox.pushLine(\'\\n(Use arrow keys to navigate the menu)\');
+logBox.pushLine(\'Total pages built: \');
+totalBox.pushLine(\'building pages...\');
+totalBox.pushLine(\`\\nTotal built: \${calculateSumCounts(directoryCounts)}\`);
+
+/**
+ * Calculates the sum of all the values in the directoryCounts object.
+ * @param {object} countsObject - The object containing the directory counts.
+ * @returns {number} - The sum of all the counts.
+ */
+function calculateSumCounts(countsObject) {
+  let sum = 0;
+  for (const key in countsObject) {
+    if (Object.prototype.hasOwnProperty.call(countsObject, key)) {
+      sum += countsObject[key];
+    }
+  }
+  return sum;
+}
+
+/**
+ * Handles messages from the parent process.
+ * If the message type is \'dataFromParent\', it updates the directoryCounts object
+ * and the total pages built display.
+ * If the message type is \'config\', it updates the menu items.
+ * @param {object} message - The message from the parent process.
+ */
+process.on(\'message\', (message) => {
+  const { type, count, dir, configArray } = message;
+  resetTimeout();
+
+  if (type === \'dataFromParent\') {
+    const shortenedPath = path.relative(__dirname, dir);
+    const newPathArray = shortenedPath.split(path.sep);
+    const newPath = path.sep + path.join(...newPathArray.slice(3));
+    directoryCounts[newPath] = count;
+    totalBox.setContent(\'\');
+    totalBox.pushLine(\'\\nbuilding pages...\');
+    totalBox.pushLine(\`\\nTotal built: \${calculateSumCounts(directoryCounts)}\`);
+    screen.render();
+    process.send({
+      type: \'dataProcessed\',
+      result: \'Data processed successfully\',
+    });
+  } else if (type === \'config\') {
+    console.log(\`Received data from parent \${configArray}\`);
+    menu.clearItems();
+    menu.setItems(configArray);
+    screen.render();
+    process.send({
+      type: \'dataProcessed\',
+      result: \'Config updated successfully\',
+    });
+  }
+});
+
+// Focus on the menu to enable keyboard navigation
+menu.focus();
+screen.render();
+process.send({ type: \'childReady\' });
+`;
 
 /**
  * Resets the timeout to 15 seconds.
@@ -59,7 +270,8 @@ process.stdout.write = process.stderr.write = stdoutLog.write.bind(stdoutLog);
 console.log('Stream redirection setup complete.');
 
 // Fork the blessed.js process
-const blessedAppProcess = fork(path.join(__dirname, 'blessed.js'));
+const blessedAppProcess = spawn('node', ['-e', childCode]);
+
 
 // Handle messages from the forked process
 blessedAppProcess.on('message', ({ type }) => {
