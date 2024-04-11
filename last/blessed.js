0061eb7ea9a94b3887b0a432311566fd9b77bddf
phukon <hello@rikiphukon.com>
2024-04-11 23:17:40 +0530

    child process

---
+++ last/blessed.js
@@ -0,0 +1,171 @@
+import blessed from 'blessed';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// Global store to hold counts for each directory
+const directoryCounts = {};
+
+const screen = blessed.screen({
+  smartCSR: true,
+  title: 'Terminal Interface',
+});
+
+const menuBox = blessed.box({
+  parent: screen,
+  width: '80%',
+  height: '100%',
+  border: {
+    type: 'line',
+  },
+  style: {
+    fg: 'white',
+    border: {
+      fg: 'white',
+    },
+  },
+});
+
+const menu = blessed.list({
+  parent: menuBox,
+  top: 'center',
+  left: 'center',
+  width: '90%',
+  height: '90%',
+  align: 'center',
+  tags: true,
+  keys: true,
+  border: {
+    type: 'line',
+  },
+  style: {
+    selected: {
+      bg: 'yellow',
+      bold: true,
+    },
+  },
+  items: ['Use the', 'arrow keys', 'to move'],
+});
+
+menuBox.pushLine('\t\tTemplate pages');
+
+const logBox = blessed.box({
+  parent: screen,
+  left: '80%',
+  width: '20%',
+  height: '80%',
+  border: {
+    type: 'line',
+  },
+  scrollbar: {
+    ch: ' ',
+    inverse: true,
+  },
+  style: {
+    fg: 'white',
+    bg: 'black',
+    border: {
+      fg: 'white',
+    },
+  },
+});
+
+const totalBox = blessed.box({
+  parent: screen,
+  top: '80%',
+  left: '80%',
+  width: '20%',
+  height: '20%',
+  border: {
+    type: 'line',
+  },
+  scrollbar: {
+    ch: ' ',
+    inverse: true,
+  },
+  style: {
+    fg: 'white',
+    bg: 'black',
+    border: {
+      fg: 'white',
+    },
+  },
+});
+
+function logMessage(message) {
+  logBox.setContent('');
+  logBox.pushLine(message);
+  screen.render();
+}
+// Event handling for menu selection
+menu.on('select', (item) => {
+  if (item.content === 'Quit') {
+    return process.exit(0);
+  }
+  logMessage(`\nSelected route:\n ${item.content}`);
+  // Debugging: Log the keys in directoryCounts
+  // console.log('Keys in directoryCounts:', Object.keys(directoryCounts));
+  const count = directoryCounts[item.content];
+  if (count !== undefined) {
+    logMessage(`\nPages built for \n ${item.content}\n\n > ${count}`);
+  } else {
+    logMessage(`\nPages built for \n ${item.content}\n\n > 0`);
+  }
+});
+
+// Handle Escape or Q to quit
+screen.key(['escape', 'q', 'C-c'], () => {
+  return process.exit(0);
+});
+
+// Initial log message
+logBox.pushLine('\n(Use arrow keys to navigate the menu)');
+logBox.pushLine('Total pages built: ');
+totalBox.pushLine('\nbuilding pages...');
+totalBox.pushLine(`\nTotal built: ${calculateSumCounts(directoryCounts)}`);
+
+function calculateSumCounts(countsObject) {
+  let sum = 0;
+  for (const key in countsObject) {
+    if (Object.prototype.hasOwnProperty.call(countsObject, key)) {
+      sum += countsObject[key];
+    }
+  }
+  return sum;
+}
+
+process.on('message', ({ type, count, dir }) => {
+  if (type === 'dataFromParent') {
+    const shortenedPath = path.relative(__dirname, dir);
+    const newPathArray = shortenedPath.split(path.sep);
+    const newPath = path.sep + path.join(...newPathArray.slice(3));
+    directoryCounts[newPath] = count;
+    totalBox.setContent('');
+    totalBox.pushLine('\nbuilding pages...');
+    totalBox.pushLine(`\nTotal built: ${calculateSumCounts(directoryCounts)}`);
+    screen.render();
+    process.send({
+      type: 'dataProcessed',
+      result: 'Data processed successfully',
+    });
+  }
+});
+
+process.on('message', ({ type, configArray }) => {
+  if (type === 'config') {
+    console.log(`Received data from parent ${configArray}`);
+    menu.clearItems();
+    menu.setItems(configArray);
+    screen.render();
+    process.send({
+      type: 'dataProcessed',
+      result: 'Config updated successfully',
+    });
+  }
+});
+
+// Focus on the menu to enable keyboard navigation
+menu.focus();
+// Render the screen
+screen.render();
+process.send({ type: 'childReady' });
--- last/watcher.js
+++ last/watcher.js
@@ -1,167 +1,50 @@
 import fs from 'fs';
 import path from 'path';
+import { fork } from 'child_process';
 import { fileURLToPath } from 'url';
 import chokidar from 'chokidar';
-// import { calculateProgress } from './progressCalculator.js';
 import { waitForDirectoryCreation } from './wait-creation.js';
 import { countDirectories } from './countDir.js';
-
 import EventEmitter from 'events';
-import blessed from 'blessed';
-// Global store to hold counts for each directory
-const directoryCounts = {};
+
+
+class ParentEmitter extends EventEmitter {}
+const parentEmitter = new ParentEmitter();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const myEmitter = new EventEmitter();
 
-const screen = blessed.screen({
-  smartCSR: true,
-  title: 'Terminal Interface',
-});
-
-const menuBox = blessed.box({
-  parent: screen,
-  width: '80%',
-  height: '100%',
-  border: {
-    type: 'line',
-  },
-  style: {
-    fg: 'white',
-    border: {
-      fg: 'white',
-    },
-  },
-});
-
-const menu = blessed.list({
-  parent: menuBox,
-  top: 'center',
-  left: 'center',
-  width: '90%',
-  height: '90%',
-  align: 'center',
-  tags: true,
-  keys: true,
-  border: {
-    type: 'line',
-  },
-  style: {
-    selected: {
-      bg: 'yellow',
-      bold: true,
-    },
-  },
-  items: ['Use the', 'arrow keys', 'to move'],
-});
-
-menuBox.pushLine('\t\tTemplate pages');
-
-const logBox = blessed.box({
-  parent: screen,
-  left: '80%',
-  width: '20%',
-  height: '80%',
-  border: {
-    type: 'line',
-  },
-  scrollbar: {
-    ch: ' ',
-    inverse: true,
-  },
-  style: {
-    fg: 'white',
-    bg: 'black',
-    border: {
-      fg: 'white',
-    },
-  },
-});
-
-const totalBox = blessed.box({
-  parent: screen,
-  top: '80%',
-  left: '80%',
-  width: '20%',
-  height: '20%',
-  border: {
-    type: 'line',
-  },
-  scrollbar: {
-    ch: ' ',
-    inverse: true,
-  },
-  style: {
-    fg: 'white',
-    bg: 'black',
-    border: {
-      fg: 'white',
-    },
-  },
-});
-
-function logMessage(message) {
-  logBox.setContent('');
-  logBox.pushLine(message);
-  screen.render();
-}
-// Event handling for menu selection
-menu.on('select', (item) => {
-  if (item.content === 'Quit') {
-    return process.exit(0);
+// ------------------- redirection
+const ensureDirectoryExists = (directory) => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
   }
-  logMessage(`\nSelected route:\n ${item.content}`);
-    // Debugging: Log the keys in directoryCounts
-  // console.log('Keys in directoryCounts:', Object.keys(directoryCounts));
-  const count = directoryCounts[item.content];
-  if (count !== undefined) {
-    logMessage(`\nPages built for \n ${item.content}\n\n > ${count}`);
-  } else {
-    logMessage(`\nPages built for \n ${item.content}\n\n > 0`);
-  }
+};
+
+// Directory paths for log file
+const logsDirectory = path.join(__dirname, 'logs');
+ensureDirectoryExists(logsDirectory);
+const stdoutLogPath = path.join(logsDirectory, 'stdout.log');
+const stdoutLog = fs.createWriteStream(stdoutLogPath);
+stdoutLog.on('error', (err) => {
+  console.error('Error writing to stdout.log:', err);
 });
 
-// Handle Escape or Q to quit
-screen.key(['escape', 'q', 'C-c'], () => {
-  return process.exit(0);
+// Redirect stdout and stderr to log files
+console.log('Redirecting stdout and stderr...');
+// process.stdout.pipe(stdoutLog);
+process.stdout.write = process.stderr.write = stdoutLog.write.bind(stdoutLog);
+// process.stdout.write = stdoutLog.write.bind(stdoutLog);
+
+console.log('Stream redirection setup complete.');
+
+// --------------
+const blessedAppProcess = fork(path.join(__dirname, 'blessed.js'));
+blessedAppProcess.on('message', ({ type, eventName, args }) => {
+  console.log(`Message from Blessed app: ${type}`);
 });
 
-// Initial log message
-logBox.pushLine('\n(Use arrow keys to navigate the menu)');
-logBox.pushLine('Total pages built: ');
-totalBox.pushLine('\nbuilding pages... ..');
-totalBox.pushLine(`\nTotal built: ${calculateSumCounts(directoryCounts)}`);
-
-function calculateSumCounts(countsObject) {
-  let sum = 0;
-  for (const key in countsObject) {
-    if (Object.prototype.hasOwnProperty.call(countsObject, key)) {
-      sum += countsObject[key];
-    }
-  }
-  return sum;
-}
-
-// Event listener for counting directories
-myEmitter.on('message', (c, d) => {
-  // Shorten the path to use as the key in directoryCounts
-  const shortenedPath = path.relative(__dirname, d);
-  // console.log('sp', shortenedPath);
-  const newPathArray = shortenedPath.split(path.sep);
-  // console.log('newpatharry', newPathArray);
-  const newPath = path.sep + path.join(...newPathArray.slice(3));
-  // console.log('newpath', newPath);
-  directoryCounts[newPath] = c;
-  // console.log('dircount', directoryCounts);
-  totalBox.setContent('');
-  totalBox.pushLine('\nbuilding pages... ..');
-  totalBox.pushLine(`\nTotal built: ${calculateSumCounts(directoryCounts)}`);
-  screen.render()
+parentEmitter.on('message', (count, dir) => {
+  blessedAppProcess.send({ type: 'dataFromParent', count, dir });
 });
-
-// Focus on the menu to enable keyboard navigation
-menu.focus();
-// Render the screen
-screen.render();
 
 /**
  * Return the list of template dirs to be watched
@@ -205,8 +88,12 @@ export async function watchDirectory(
     const joinedPaths = pathArrays.map((p) => path.sep + path.join(...p));
 
     const watchList = await dirWatchList(targetDirectory, joinedPaths);
-    menu.clearItems();
-    menu.setItems([...joinedPaths, ' ', 'Quit']);
+    blessedAppProcess.on('message', ({ type }) => {
+      if (type === 'childReady') {
+        const updatedOptions = [...joinedPaths, ' ', 'Quit'];
+        blessedAppProcess.send({ type: 'config', configArray: updatedOptions });
+      }
+    });
 
     const watchers = [];
 
@@ -217,7 +104,7 @@ export async function watchDirectory(
       watcher.on('addDir', async () => {
         // console.log('dir', dir);
         let count = await countDirectories(dir);
-        myEmitter.emit('message', count, dir);
+        parentEmitter.emit('message', count, dir);
       });
     }
   } catch (err) {
@@ -225,75 +112,3 @@ export async function watchDirectory(
     return [];
   }
 }
-// export async function watchDirectory(directoryPath, dirs, onProgressUpdate, onStopCallback) {
-// 	const __dirname = path.dirname(fileURLToPath(import.meta.url));
-// 	const targetDirectory = path.resolve(__dirname, '../fgh/play');
-
-// 	await waitForDirectoryCreation(directoryPath);
-// 	try {
-// 		const pathArrays = Array.from(dirs).map((path) => {
-// 			const normalizedPath = path.replace(/\\/g, '/');
-// 			const pathArray = normalizedPath.split('/');
-// 			pathArray.pop();
-// 			return pathArray.slice(2);
-// 		});
-
-// 		// /!\ Returns a set!
-// 		const watchList = await dirWatchList(
-// 			targetDirectory,
-// 			pathArrays.map((p) => '\\' + path.join(...p))
-// 		);
-
-// 		const watchers = [];
-
-// 		for (const dir of watchList) {
-// 			const watcher = chokidar.watch(dir, { ignoreInitial: true });
-// 			watchers.push(watcher);
-
-// 			watcher.on('add', async () => {
-// 				// const currentFiles = await contentCounter(dir);
-// 				// const progress = calculateProgress(currentFiles, totalFiles);
-// 				// onProgressUpdate(progress);
-// 				// if (progress >= 100) {
-// 				//   watcher.close();
-// 				//   if (typeof onStopCallback === 'function') {
-// 				//     onStopCallback();
-// 				//   }
-// 				// }
-// 			});
-// 		}
-
-// 	} catch (err) {
-// 		console.error('Error:', err);
-// 	}
-
-// 	// const currentFiles = await contentCounter(directoryPath);
-// 	// const initialProgress = calculateProgress(currentFiles, totalFiles);
-
-// 	// if (initialProgress >= 100) {
-// 	// 	// console.log('\nProgress already at 100% or greater');
-// 	// 	if (typeof onStopCallback === 'function') {
-// 	// 		onStopCallback();
-// 	// 	}
-// 	// 	return;
-// 	// }
-// 	const templateDirWatcher = chokidar.watch(directoryPath, { ignoreInitial: true });
-// 	const watcher = chokidar.watch(directoryPath, { ignoreInitial: true });
-
-// 	async function updateProgress() {
-// 		// const currentFiles = await contentCounter(directoryPath);
-// 		// const progress = calculateProgress(currentFiles, totalFiles);
-// 		onProgressUpdate('dd');
-
-// 		// if (progress >= 100) {
-// 		// 	watcher.close();
-
-// 		// 	if (typeof onStopCallback === 'function') {
-// 		// 		onStopCallback();
-// 		// 	}
-// 		// }
-// 	}
-
-// 	watcher.on('add', updateProgress);
-// 	watcher.on('addDir', updateProgress);
-// }
